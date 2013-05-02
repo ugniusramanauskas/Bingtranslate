3d1e847bdf13bc1fb246cf8394f7c5723362b5af
Ugnius R <ugnius.ramanauskas@gmail.com>
2013-05-02 21:23:08 +0200

    first commit

---
+++ config/module.config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Bingtranslate\Controller\Bingtranslate' => 'Bingtranslate\Controller\BingtranslateController',
+        ),
+    ),
+
+    // The following section is new and should be added to your file
+
+);
